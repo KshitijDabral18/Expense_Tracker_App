cded34543ff42e91ed12c67b4dce59c874cfa7d5
KshitijDabral18 <kshitijdabral1999@gmail.com>
2025-08-15 08:17:35 +0530

    CustomUserDetails added

--- app/src/main/java/org/ExpenseTracker/Services/CustomUserDetails.java
+++ app/src/main/java/org/ExpenseTracker/Services/CustomUserDetails.java
@@ -1,9 +1,12 @@
 package org.ExpenseTracker.Services;
 
 import org.ExpenseTracker.Entities.UserInfo;
+import org.ExpenseTracker.Entities.UserRoles;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -12,6 +15,16 @@ public class CustomUserDetails extends UserInfo implements UserDetails {
     private String username;
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(UserInfo byUsername) {
+        this.username = byUsername.getUsername();
+        this.password = byUsername.getPassword();
+        List<GrantedAuthority> auths = new ArrayList<>();
+        for (UserRoles role: byUsername.getRoles()) {
+            auths.add(new SimpleGrantedAuthority(role.getName().toUpperCase()));
+        }
+        this .authorities = auths;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
